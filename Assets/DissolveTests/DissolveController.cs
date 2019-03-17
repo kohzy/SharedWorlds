888231741f79545d8104530ce59033f37a58f757
kohzy <kohzhengyang@gmail.com>
2019-03-17 15:50:11 -0400

    spotlight reveal

---
+++ Assets/DissolveTests/DissolveController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Shader.SetGlobalVector("_WandPos", transform.position);
+    }
+}
